ec7d613e642a24f33aa2faebde106a62b625ac0e
IrinaZinchenko <zinahohol@gmail.com>
2023-11-14 20:35:04 +0300

    added HTML-markup draft

---
+++ copy-project-js/src/mock/data.js
@@ -0,0 +1,355 @@
+const data = {
+  "articleIds": [
+    "773728",
+    "773716",
+    "773698",
+    "773700",
+    "773694"
+  ],
+  "articleRefs": {
+    "773694": {
+      "id": "773694",
+      "timePublished": "2023-11-13T17:23:49+00:00",
+      "isCorporative": false,
+      "lang": "ru",
+      "titleHtml": "Учёные симулировали чёрную дыру и излучение Хокинга в лаборатории",
+      "editorVersion": "2.0",
+      "postType": "news",
+      "postLabels": [],
+      "author": {
+        "id": "60351",
+        "alias": "SLY_G",
+        "fullname": "Вячеслав Голованов",
+        "avatarUrl": "//habrastorage.org/getpro/habr/avatars/329/fc8/9e5/329fc89e55a9132bf200ff315065f3f7.jpg",
+        "speciality": "Научпоп. Проповедую в храме науки."
+      },
+      "statistics": {
+        "commentsCount": 0,
+        "favoritesCount": 0,
+        "readingCount": 1364,
+        "score": 2,
+        "votesCount": 4,
+        "votesCountPlus": 3,
+        "votesCountMinus": 1
+      },
+      "hubs": [
+        {
+          "id": "21910",
+          "alias": "popular_science",
+          "type": "collective",
+          "title": "Научно-популярное",
+          "titleHtml": "Научно-популярное",
+          "isProfiled": false
+        },
+        {
+          "id": "21968",
+          "alias": "physics",
+          "type": "collective",
+          "title": "Физика",
+          "titleHtml": "Физика",
+          "isProfiled": false
+        }
+      ],
+      "flows": [
+        {
+          "id": "7",
+          "alias": "popsci",
+          "title": "Научпоп",
+          "titleHtml": "Научпоп"
+        }
+      ],
+      "relatedData": null,
+      "leadData": {
+        "textHtml": "<p>Используя цепочку атомов, расположенных в один ряд, для моделирования горизонта событий чёрной дыры, группа физиков в 2022 г. наблюдала эквивалент того, что мы называем излучением Хокинга, - частицы, рождающиеся в результате возмущений квантовых флуктуаций, вызванных разрывом пространства-времени чёрной дыры.</p><p>Это, по их мнению, может помочь разрешить противоречие между двумя непримиримыми в настоящее время системами описания Вселенной: общей теорией относительности, которая описывает поведение гравитации как непрерывного поля, известного как пространство-время, и квантовой механикой, которая описывает поведение дискретных частиц с помощью математики вероятности.</p>",
+        "imageUrl": "https://habrastorage.org/getpro/habr/upload_files/3b3/5c2/b9b/3b35c2b9bd31fb1c4776fb84f206e344.jpg",
+        "buttonTextHtml": "Читать далее",
+        "image": {
+          "url": "https://habrastorage.org/getpro/habr/upload_files/3b3/5c2/b9b/3b35c2b9bd31fb1c4776fb84f206e344.jpg",
+          "fit": "cover",
+          "positionY": 0,
+          "positionX": 0
+        }
+      },
+      "status": "published",
+      "plannedPublishTime": null,
+      "checked": null,
+      "format": null,
+      "readingTime": null,
+      "complexity": null
+    },
+    "773698": {
+      "id": "773698",
+      "timePublished": "2023-11-13T18:26:02+00:00",
+      "isCorporative": false,
+      "lang": "ru",
+      "titleHtml": "Уэбб помог уточнить детали процесса формирования планет",
+      "editorVersion": "2.0",
+      "postType": "news",
+      "postLabels": [],
+      "author": {
+        "id": "60351",
+        "alias": "SLY_G",
+        "fullname": "Вячеслав Голованов",
+        "avatarUrl": "//habrastorage.org/getpro/habr/avatars/329/fc8/9e5/329fc89e55a9132bf200ff315065f3f7.jpg",
+        "speciality": "Научпоп. Проповедую в храме науки."
+      },
+      "statistics": {
+        "commentsCount": 0,
+        "favoritesCount": 0,
+        "readingCount": 351,
+        "score": 2,
+        "votesCount": 2,
+        "votesCountPlus": 2,
+        "votesCountMinus": 0
+      },
+      "hubs": [
+        {
+          "id": "21910",
+          "alias": "popular_science",
+          "type": "collective",
+          "title": "Научно-популярное",
+          "titleHtml": "Научно-популярное",
+          "isProfiled": false
+        },
+        {
+          "id": "22022",
+          "alias": "astronomy",
+          "type": "collective",
+          "title": "Астрономия",
+          "titleHtml": "Астрономия",
+          "isProfiled": false
+        }
+      ],
+      "flows": [
+        {
+          "id": "7",
+          "alias": "popsci",
+          "title": "Научпоп",
+          "titleHtml": "Научпоп"
+        }
+      ],
+      "relatedData": null,
+      "leadData": {
+        "textHtml": "<p>Космический телескоп Джеймс Уэбб <a href=\"https://iopscience.iop.org/article/10.3847/2041-8213/acf5ec\" rel=\"noopener noreferrer nofollow\">обнаружил</a> важнейшее доказательство в поддержку давней гипотезы образования планет, что даёт астрономам уверенность в том, что они правильно поняли одну из составляющих космического процесса.</p><p>Данные Уэбба, обработанные международной группой исследователей, подтверждают теорию \"дрейфа ледяной гальки\", которая, как считается, играет важную роль в объединении пыли и камней, которые в конечном итоге превращаются в планеты, подобные нашей.</p><p>Проще говоря, дрейф ледяных камешков происходит следующим образом: когда крошечные кусочки материала, покрытые льдом, сталкиваются друг с другом во внешней части молодого протопланетного диска, они теряют импульс, что позволяет им падать к звезде в более тёплую зону, где их замёрзшее покрытие сублимируется.</p>",
+        "imageUrl": "https://habrastorage.org/getpro/habr/upload_files/024/dab/ba1/024dabba1ffc7169a05fae0284092fa3.jpg",
+        "buttonTextHtml": "Читать далее",
+        "image": {
+          "url": "https://habrastorage.org/getpro/habr/upload_files/024/dab/ba1/024dabba1ffc7169a05fae0284092fa3.jpg",
+          "fit": "cover",
+          "positionY": 0,
+          "positionX": 45
+        }
+      },
+      "status": "published",
+      "plannedPublishTime": null,
+      "checked": null,
+      "format": null,
+      "readingTime": null,
+      "complexity": null
+    },
+    "773700": {
+      "id": "773700",
+      "timePublished": "2023-11-13T17:49:43+00:00",
+      "isCorporative": false,
+      "lang": "ru",
+      "titleHtml": "ГК «Бештау» создала межрегиональный кластер радиоэлектронной промышленности",
+      "editorVersion": "2.0",
+      "postType": "news",
+      "postLabels": [],
+      "author": {
+        "id": "2746691",
+        "alias": "IgnatChuker",
+        "fullname": "Игнатий Цукергохер",
+        "avatarUrl": "//habrastorage.org/getpro/habr/avatars/d4a/f7a/f80/d4af7af80f3091531c34ca3a18aa6be7.jpg",
+        "speciality": "Информационная служба Хабра"
+      },
+      "statistics": {
+        "commentsCount": 1,
+        "favoritesCount": 0,
+        "readingCount": 538,
+        "score": 3,
+        "votesCount": 3,
+        "votesCountPlus": 3,
+        "votesCountMinus": 0
+      },
+      "hubs": [
+        {
+          "id": "20754",
+          "alias": "tech_events",
+          "type": "collective",
+          "title": "Конференции",
+          "titleHtml": "Конференции",
+          "isProfiled": false
+        },
+        {
+          "id": "21484",
+          "alias": "electronics",
+          "type": "collective",
+          "title": "Производство и разработка электроники",
+          "titleHtml": "Производство и разработка электроники",
+          "isProfiled": true
+        }
+      ],
+      "flows": [
+        {
+          "id": "1",
+          "alias": "develop",
+          "title": "Разработка",
+          "titleHtml": "Разработка"
+        },
+        {
+          "id": "4",
+          "alias": "marketing",
+          "title": "Маркетинг",
+          "titleHtml": "Маркетинг"
+        }
+      ],
+      "relatedData": null,
+      "leadData": {
+        "textHtml": "<p>Группа компаний (ГК) «Бештау» <a href=\"https://gkbeshtau.ru/news/mezhregionalnyj-klaster-radioelektronnoj-promyshlennosti-beshtau-v-trojke-luchshih\" rel=\"noopener noreferrer nofollow\">представила</a> проект межрегионального кластера радиоэлектронной промышленности в&nbsp;рамках защиты проектов по Федеральной программе «Proкластеры» Минпромторга России. По&nbsp;итогам защиты проект получил третье место.</p><p>Цель создания кластера&nbsp;— обеспечение производства импортозамещающей промышленной продукции с&nbsp;максимальным уровнем локализации. Идея создания в&nbsp;Ростовской области ядра межрегионального кластера радиоэлектронной промышленности&nbsp;была озвучена в&nbsp;июле 2023&nbsp;года на&nbsp;международной выставке «Иннопром-2023». </p><p></p>",
+        "imageUrl": null,
+        "buttonTextHtml": "Читать далее",
+        "image": null
+      },
+      "status": "published",
+      "plannedPublishTime": null,
+      "checked": null,
+      "format": null,
+      "readingTime": null,
+      "complexity": null
+    },
+    "773716": {
+      "id": "773716",
+      "timePublished": "2023-11-13T20:23:53+00:00",
+      "isCorporative": false,
+      "lang": "ru",
+      "titleHtml": "РКК «Энергия» впервые представила концепцию РОС на международной выставке",
+      "editorVersion": "2.0",
+      "postType": "news",
+      "postLabels": [],
+      "author": {
+        "id": "2082889",
+        "alias": "AnnieBronson",
+        "fullname": null,
+        "avatarUrl": "//habrastorage.org/getpro/habr/avatars/6fb/245/fc1/6fb245fc1e1db1b13509119680269375.jpg",
+        "speciality": "Информационная служба Хабра"
+      },
+      "statistics": {
+        "commentsCount": 7,
+        "favoritesCount": 0,
+        "readingCount": 292,
+        "score": 3,
+        "votesCount": 3,
+        "votesCountPlus": 3,
+        "votesCountMinus": 0
+      },
+      "hubs": [
+        {
+          "id": "21910",
+          "alias": "popular_science",
+          "type": "collective",
+          "title": "Научно-популярное",
+          "titleHtml": "Научно-популярное",
+          "isProfiled": false
+        },
+        {
+          "id": "21962",
+          "alias": "space",
+          "type": "collective",
+          "title": "Космонавтика",
+          "titleHtml": "Космонавтика",
+          "isProfiled": false
+        }
+      ],
+      "flows": [
+        {
+          "id": "7",
+          "alias": "popsci",
+          "title": "Научпоп",
+          "titleHtml": "Научпоп"
+        }
+      ],
+      "relatedData": null,
+      "leadData": {
+        "textHtml": "<p>РКК «Энергия» впервые на международной площадке Dubai Airshow представила концепцию Российской орбитальной станции и коммерческого модуля.</p><p>Как указывает РКК «Энергия», архитектура станции позволяет создавать практически неограниченное количество модулей для различных задач и пользователей. Коммерческий модуль при этом можно не только интегрировать в основную конструкцию станции, но и использовать его как обслуживаемый отдельно летающий модуль.&nbsp;</p><p></p>",
+        "imageUrl": "https://habrastorage.org/getpro/habr/upload_files/a18/278/e64/a18278e64064d0a2de79230aa58f30a3.jpg",
+        "buttonTextHtml": "Читать далее",
+        "image": {
+          "url": "https://habrastorage.org/getpro/habr/upload_files/a18/278/e64/a18278e64064d0a2de79230aa58f30a3.jpg",
+          "fit": "cover",
+          "positionY": 0,
+          "positionX": 0
+        }
+      },
+      "status": "published",
+      "plannedPublishTime": null,
+      "checked": null,
+      "format": null,
+      "readingTime": null,
+      "complexity": null
+    },
+    "773728": {
+      "id": "773728",
+      "timePublished": "2023-11-13T21:35:38+00:00",
+      "isCorporative": false,
+      "lang": "ru",
+      "titleHtml": "У Земли нашли загадочное «сердцебиение», которое происходит каждые ~27 млн лет",
+      "editorVersion": "2.0",
+      "postType": "news",
+      "postLabels": [],
+      "author": {
+        "id": "60351",
+        "alias": "SLY_G",
+        "fullname": "Вячеслав Голованов",
+        "avatarUrl": "//habrastorage.org/getpro/habr/avatars/329/fc8/9e5/329fc89e55a9132bf200ff315065f3f7.jpg",
+        "speciality": "Научпоп. Проповедую в храме науки."
+      },
+      "statistics": {
+        "commentsCount": 0,
+        "favoritesCount": 0,
+        "readingCount": 0,
+        "score": 0,
+        "votesCount": 0,
+        "votesCountPlus": 0,
+        "votesCountMinus": 0
+      },
+      "hubs": [
+        {
+          "id": "21910",
+          "alias": "popular_science",
+          "type": "collective",
+          "title": "Научно-популярное",
+          "titleHtml": "Научно-популярное",
+          "isProfiled": false
+        }
+      ],
+      "flows": [
+        {
+          "id": "7",
+          "alias": "popsci",
+          "title": "Научпоп",
+          "titleHtml": "Научпоп"
+        }
+      ],
+      "relatedData": null,
+      "leadData": {
+        "textHtml": "<p>За последние 260 млн лет появились и исчезли динозавры, Пангея разделилась на континенты и острова, которые мы видим сегодня, а человек быстро и необратимо изменил мир, в котором мы живём.</p><p>Но, похоже, Земля не теряла времени. Исследования древних геологических событий показывают, что на нашей планете каждые 27 млн лет или около того происходит медленное, устойчивое \"сердцебиение\" геологической активности.</p><p>Этот пульс сгруппированных геологических событий - вулканической активности, массовых вымираний, реорганизации плит и повышения уровня моря - невероятно медленный, 27,5-миллионнолетний цикл катастрофических подъёмов и спадов. Но, к счастью для нас, исследователи считают, что до следующего \"импульса\" ещё 20 млн лет.</p>",
+        "imageUrl": "https://habrastorage.org/getpro/habr/upload_files/bc8/a60/6fd/bc8a606fd19e29652a35c63756f7d435.jpg",
+        "buttonTextHtml": "Читать далее",
+        "image": {
+          "url": "https://habrastorage.org/getpro/habr/upload_files/bc8/a60/6fd/bc8a606fd19e29652a35c63756f7d435.jpg",
+          "fit": "cover",
+          "positionY": 0,
+          "positionX": 50
+        }
+      },
+      "status": "published",
+      "plannedPublishTime": null,
+      "checked": null,
+      "format": null,
+      "readingTime": null,
+      "complexity": null
+    }
+  },
+  "pagesCount": 10
+}
